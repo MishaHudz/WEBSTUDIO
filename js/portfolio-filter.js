5cfeecd80ec1308ea6a7bdc90b4153902bf1d0e9
MishaHudz <misha.hudz@gmail.com>
2023-06-02 21:10:17 +0300

    code optimization

--- js/portfolio-filter.js
+++ js/portfolio-filter.js
@@ -1,14 +1,20 @@
-const filterButtons = document.querySelector('.filter');
-const arrOfCartMarcers = document.querySelectorAll('.project-text');
+const refs = {
+  filterButtons: document.querySelector('.filter'),
+  arrOfCartMarcers: document.querySelectorAll('.project-text'),
+};
+
+const { filterButtons, arrOfCartMarcers } = refs;
 
 filterButtons.addEventListener('click', onFilterButttonClick);
 
 function onFilterButttonClick(evt) {
   if (evt.target.type !== 'button') return;
 
-  const filter = evt.target.innerHTML.toLowerCase();
+  filterPortfolioCards(arrOfCartMarcers, evt.target.innerHTML.toLowerCase());
+}
 
-  arrOfCartMarcers.forEach(cartMarcer => {
+function filterPortfolioCards(arrOfFilterParagraps, filter) {
+  arrOfFilterParagraps.forEach(cartMarcer => {
     cartMarcer.closest('li').classList.remove('hide-portfolio-elem');
 
     if (cartMarcer.innerHTML.toLowerCase() !== filter && filter !== 'all')
